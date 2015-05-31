772b3f3a2193d75004a97892c0d6567f53ee3f34
Nick Stark <thors1982@gmail.com>
2015-05-31 02:44:46 -0400

    More vagrant setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+#Install MongoDB
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 apt-get update
+
+apt-get install -y mongodb-org
 
 #Install Apache
 apt-get install -y apache2
@@ -17,8 +20,10 @@ mkdir mpgTrackerApi
 cd mpgTrackerApi
 virtualenv venv
 . venv/bin/activate
-sudo pip install Flask
+sudo pip install flask-restful
+sudo pip install pymongo
+sudo pip install flask-pymongo
 
-#Install MongoDB
-apt-get install -y mongodb-org
+#Run api
+python /vagrant/api.py
 
